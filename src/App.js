3a3bd9b4450d9aa963375ffa0bd1cf6f5616475a
HSL <originalkimchi100@gmail.com>
2024-10-21 02:41:38 +0900

    image commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import MyButton from './components/logobutton.js';
-
+import Header from './components/logobutton.js';
+import Home  from './components/home.js';
 function App() {
   return (
     
     <div className="App">
-      <MyButton />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Header />
+      <Home />
+      <Home />
+
     </div>
   );
 }
