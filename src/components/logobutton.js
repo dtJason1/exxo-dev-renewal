497cb3e306180e2abe5d16e0d21ef7d7ecf7d0a4
HSL <originalkimchi100@gmail.com>
2024-10-21 02:40:27 +0900

    logobutton commit

--- src/components/logobutton.js
+++ src/components/logobutton.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import '../App.css';
+import image_1 from "../image/logo.png";
 
-function MyButton() {
-    return (
-      <button>I'm a butsdffton</button>
-    );
-  }
 
-export default MyButton;
+
+function divBox(){
+  return(
+    <div>
+      <h1>EXXO BIO</h1>
+
+    </div>
+  );
+}
+
+function Header() {
+  const myFunction = () => {
+    console.log("hello");
+  };  
+  return (
+    <div className='header'>
+      <button className='img-button'><img src={image_1} className='logobutton' alt="my image" onClick={myFunction} /></button>
+    </div>
+  );
+}
+
+export default Header;
