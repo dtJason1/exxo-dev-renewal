a742df792700b4d91f62a6693e4714d3ebc376c7
HSL <originalkimchi100@gmail.com>
2024-10-21 02:32:44 +0900

    divbox commit

---
+++ src/components/divbox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function DivBox() {
+    return (
+        <div className='div1'>  
+            <h1>
+                EXXO BIO
+            </h1>
+            <h1>
+                EXXO BIO
+            </h1>
+        </div>
+    );
+  }
+  
+  export default DivBox;
