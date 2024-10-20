31b590462fa667cb56f11c17fdc99d6498a51608
HSL <originalkimchi100@gmail.com>
2024-10-20 18:06:52 +0900

    add logobutton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from './components/logobutton.js';
 
 function App() {
   return (
+    
     <div className="App">
+      <MyButton />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
---
+++ src/components/logobutton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MyButton() {
+    return (
+      <button>I'm a butsdffton</button>
+    );
+  }
+
+export default MyButton;
