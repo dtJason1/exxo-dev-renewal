5b3b9ed51caad6664ce44715535964968070ef0f
jason <originalkimchi100@gmail.com>
2024-10-21 12:42:50 +0900

    main file commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/logobutton.js';
 import Home  from './components/home.js';
+import {useState, useEffect,handleNavigation, useCallback, useRef} from  'react';
 function App() {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll); //clean up
+    };
+  }, []);
+
+  const handleScroll = () => {
+    console.log('scrolled');
+  };
+
   return (
-    
+
     <div className="App">
+
       <Header />
+      <Home />
       <Home />
     </div>
   );
 }
 
 export default App;
+
+
+
