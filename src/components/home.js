b2612d2f1642a4e2189015ab696349eeda17f8c4
jason <originalkimchi100@gmail.com>
2024-10-21 12:41:59 +0900

    homepage commit

--- src/components/home.js
+++ src/components/home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../App.css';
 import image_1 from "../image/image_1.jpg";
 import DivBox from './divbox';
-
+import useScrollFadeIn from './divbox';
 function Home() {
     return (
     <section>
       <div className='home'>  
         <DivBox/>
+        <useScrollFadeIn/>
         <img className='homeimage' src={image_1} /> 
         
       </div>
